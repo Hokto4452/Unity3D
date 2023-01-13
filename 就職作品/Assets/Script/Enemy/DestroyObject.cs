51f83c521c1317a1c173e5735dbad379298e5b04
北川湧斗 <97575758+Hokto4452@users.noreply.github.com>
2023-01-13 17:16:16 +0900

    エネミーHP、エネミー消去のスクリプト　完成 ※エネミーの作成はまだ

---
+++ 就職作品/Assets/Script/Enemy/DestroyObject.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    public int damage;          //当たった部位毎のダメージ量
+    private GameObject enemy2;  //敵オブジェクト
+    private EnemyHP _hitPoint;         //HPクラス
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemy2 = GameObject.Find("Enemy");   //敵情報を取得
+        _hitPoint = enemy2.GetComponent<EnemyHP>();      //HP情報を取得
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("bullet"))
+        {
+            //HPクラスのDamage関数を呼び出す
+            _hitPoint.Damage(damage);
+
+            //ぶつかってきたオブジェクトを破壊する.
+            Destroy(other.gameObject);
+        }
+    }
+}
--- 就職作品/Assets/Script/Enemy/EnemyHP.cs
+++ 就職作品/Assets/Script/Enemy/EnemyHP.cs
@@ -6,19 +6,13 @@ public class EnemyHP : MonoBehaviour
 {
     public int _hitPoint = 100;         //HP
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         EnemyHp();
     }
 
-    public void EnemyHp()
+    void EnemyHp()
     {
         if (_hitPoint <= 0)         //HP‚ª0‚É‚È‚Á‚½Žž
         {
