636bd0921a6609e4cc1a7d908164969e9e6eb946
北川湧斗 <97575758+Hokto4452@users.noreply.github.com>
2023-02-03 14:05:04 +0900

    バグ取り

--- 就職作品/Assets/SciFi Warehouse Kit/Demo/Scripts/PlayRandomSound.cs
+++ 就職作品/Assets/SciFi Warehouse Kit/Demo/Scripts/PlayRandomSound.cs
@@ -3,27 +3,27 @@
  
  public class PlayRandomSound : MonoBehaviour {
  
-     public AudioSource randomSound;
-     public AudioClip[] audioSources;
+     //public AudioSource randomSound;
+     //public AudioClip[] audioSources;
 
-     public int clipDelay = 5;
+     //public int clipDelay = 5;
  
-     // Use this for initialization
-     void Start () {
+     //// Use this for initialization
+     //void Start () {
  
-         StartAudio ();
-     }
+     //    StartAudio ();
+     //}
  
  
-     void StartAudio()
-     {
-         Invoke ("RandomSoundness", clipDelay);
-     }
+     //void StartAudio()
+     //{
+     //    Invoke ("RandomSoundness", clipDelay);
+     //}
  
-     void RandomSoundness()
-     {
-         randomSound.clip = audioSources[Random.Range(0, audioSources.Length)];
-         randomSound.Play ();
-         StartAudio ();
-     }
+     //void RandomSoundness()
+     //{
+     //    randomSound.clip = audioSources[Random.Range(0, audioSources.Length)];
+     //    randomSound.Play ();
+     //    StartAudio ();
+     //}
  }
--- 就職作品/Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement.cs
+++ 就職作品/Assets/SciFi Warehouse Kit/Demo/Scripts/PlayerMovement.cs
@@ -15,46 +15,48 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
-    
-     public AudioClip footStepSound;
-     public float footStepDelay;
- 
-     private float nextFootstep = 0;
+
+    public AudioClip footStepSound;
+    public float footStepDelay;
+
+    private float nextFootstep = 0;
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        //isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-        if (isGrounded && velocity.y <0)
-            {
-            velocity.y = -2f;
-            }
+        //if (isGrounded && velocity.y <0)
+        //    {
+        //    velocity.y = -2f;
+        //    }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        //float x = Input.GetAxis("Horizontal");
+        //float z = Input.GetAxis("Vertical");
 
-        Vector3 motion = transform.right * x + transform.forward * z;
-        controller.Move(motion * speed * Time.deltaTime);
+        //Vector3 motion = transform.right * x + transform.forward * z;
+        //controller.Move(motion * speed * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
+        //if(Input.GetButtonDown("Jump") && isGrounded)
+        //{
+        //    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        //}
 
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        //velocity.y += gravity * Time.deltaTime;
+        //controller.Move(velocity * Time.deltaTime);
 
-         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) && isGrounded)
-            {
-             nextFootstep -= Time.deltaTime;
-             if (nextFootstep <= 0) 
-                {
-                 GetComponent<AudioSource>().PlayOneShot(footStepSound, 0.7f);
-                 nextFootstep += footStepDelay;
-                }
-             }
-         }
+        // if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.W) && isGrounded)
+        //    {
+        //     nextFootstep -= Time.deltaTime;
+        //     if (nextFootstep <= 0) 
+        //        {
+        //         GetComponent<AudioSource>().PlayOneShot(footStepSound, 0.7f);
+        //         nextFootstep += footStepDelay;
+        //        }
+        //     }
+        // }
+    }
 }
+//}
 
 
